47a6fad59a071a7031850f2fcaaa58f54c25a4e7
Bishamoda <mihailshipakin@mail.ru>
2021-01-17 22:19:12 +0300

    Калькулятор 2 в HW4

--- HW1/HW3/Калькулятор/Program.cs
+++ HW1/HW3/Калькулятор/Program.cs
@@ -67,7 +67,6 @@ namespace Калькулятор
                             break;
                     }
 
-
                 }
                 else
                 {
---
+++ HW1/HW4/Калькулятор 2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Калькулятор_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number1;
+            string number2;
+            string operation;
+            bool succes1;
+            bool succes2;
+
+            do
+            {
+                Console.WriteLine("Ну давайте считать!\n" + "Введите два числа:");
+                number1 = Console.ReadLine();
+                number2 = Console.ReadLine();
+
+                succes1 = double.TryParse(number1, out double a);
+                succes2 = double.TryParse(number2, out double b);
+
+                if ((succes1 && succes2) == true)
+                {
+                    Console.WriteLine("Ваши числа: " + a + " " + b);
+                    Console.WriteLine("Выберите операцию: + , - , * , / ");
+
+                    operation = Console.ReadLine();
+                    switch (operation)
+                    {
+                        case "+":
+                            Console.WriteLine(Sum(a, b));
+                            break;
+
+                        case "-":
+                            Console.WriteLine(Sub(a, b));
+                            break;
+
+                        case "*":
+                            Console.WriteLine(Mul(a, b));
+                            break;
+
+                        case "/":
+                            Console.WriteLine(Div(a, b));
+                            break;
+
+                        default:
+                            Console.WriteLine("Такой операции нет!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не число!");
+                }
+
+            } while ((succes1 && succes2) == true);
+        }
+
+        public static double Sum(double a, double b)
+        {
+            double result;
+            result = a + b;
+            return result;
+        }
+
+        public static double Sub(double a, double b)
+        {
+            double result;
+            result = a - b;
+            return result;
+        }
+
+        public static double Mul(double a, double b)
+        {
+            double result;
+            result = a * b;
+            return result;
+        }
+        public static double Div(double a, double b)
+        {
+            double result;
+            if (b != 0)
+            {
+                result = a / b;
+                return result;
+            }
+            else
+            {
+                return 1;
+            }
+    
+        }
+    }
+}
