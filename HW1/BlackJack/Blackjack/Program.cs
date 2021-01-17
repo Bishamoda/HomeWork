7cf629e8dab873a66e7dbaea512adc7bae1fc978
Bishamoda <mihailshipakin@mail.ru>
2021-01-17 18:46:33 +0300

    Лекция 4 + методы в BJ

--- HW1/BlackJack/Blackjack/Program.cs
+++ HW1/BlackJack/Blackjack/Program.cs
@@ -11,50 +11,138 @@ namespace Blackjack
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            
-            bool isPlayerContinue = true;
-            bool isComputerContinue = true;
+
+            int[] cards = new int[]
+            {
+                2, 3, 4, 5, 6, 7, 8, 9, 10, // - цифры
+                10, 10, 10, // - картинки
+                11, // - туз
+
+                2, 3, 4, 5, 6, 7, 8, 9, 10, // - цифры
+                10, 10, 10, // - картинки
+                11, // - туз
+
+                2, 3, 4, 5, 6, 7, 8, 9, 10, // - цифры
+                10, 10, 10, // - картинки
+                11, // - туз
+
+                2, 3, 4, 5, 6, 7, 8, 9, 10, // - цифры
+                10, 10, 10, // - картинки
+                11, // - туз
+            };
+
+            cards = Shuffle(cards);
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                Console.WriteLine(cards[i]);
+            }
+
+            Console.WriteLine();
+
+            //cards = Shuffle(cards);
+            //cards = Shuffle(cards);
+
+            //bool isPlayerContinue = true;
+            //bool isComputerContinue = true;
+
+            //Random random = new Random();
+
+            //do
+            //{
+            //    isPlayerContinue = AskPlayer(isPlayerContinue);
+
+            //    isComputerContinue = AskComputer(isComputerContinue, random);
+
+            //} while (isPlayerContinue || isComputerContinue);
+        }
+
+        private static int[] Shuffle(int[] cards)
+        {
+            int[] usedIndexes = new int[cards.Length];
+
+            for (int i = 0; i < usedIndexes.Length; i++)
+            {
+                usedIndexes[i] = -1;
+            }
+
+            int[] result = new int[cards.Length];
 
             Random random = new Random();
 
-            do
+            for (int i = 0; i < cards.Length; i++)
             {
-                Console.WriteLine("Будешь тянуть карту? (y/n)");
-                string answer = Console.ReadLine();
+                int index = 0;
 
-                if (answer == "y")
+                do
                 {
-                    Console.WriteLine("Вы тяните");
-                }
+                    index = random.Next(0, cards.Length);
 
-                if (answer == "n")
-                {
-                    Console.WriteLine("Вы пасcуете");
-                    isPlayerContinue = false;
-                }
+                } while (Contains(usedIndexes, index));
 
-                int decisionNumber = 0;
+                result[i] = cards[index];
+                usedIndexes[i] = index;
+            }
 
-                if (isComputerContinue)
-                {
-                    decisionNumber = random.Next(0, 2);
-                }
-
-                if (decisionNumber == 0)
-                {
-                    Console.WriteLine("Компьютер тянете карту");
-                }
-
-                if (decisionNumber == 1)
-                {
-                    Console.WriteLine("Компьютер пассуете");
-                    isPlayerContinue = false;
-                }
-            } while (isPlayerContinue);
-
-            
-
-            Console.ReadKey();
+            return result;
         }
+
+        private static bool Contains(int[] values, int value)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == value)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool AskComputer(bool isComputerContinue, Random random)
+        {
+            int decisionNumber = 0;
+
+            if (isComputerContinue)
+            {
+                decisionNumber = random.Next(0, 2);
+            }
+
+            if (decisionNumber == 0)
+            {
+                Console.WriteLine("Компьютер тянет карту");
+            }
+
+            if (decisionNumber == 1)
+            {
+                Console.WriteLine("Компьютер пасует");
+                isComputerContinue = false;
+            }
+
+            return isComputerContinue;
+        }
+
+        private static bool AskPlayer(bool isPlayerContinue)
+        {
+            Console.WriteLine("Будешь тянуть карту? (y/n)");
+            string answer = Console.ReadLine();
+
+            if (answer == "y")
+            {
+                Console.WriteLine("Вы тянете карту");
+            }
+
+            if (answer == "n")
+            {
+                Console.WriteLine("Вы пасуете");
+                isPlayerContinue = false;
+            }
+
+
+
+            return isPlayerContinue;
+        } 
+
     }
 }
---
+++ HW1/Lesson 4/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace Lesson_4
+{
+    class Program
+    {
+        
+        
+            //Методы всегда отражают дейсвия
+            //Поэтому названия методов - это глаголы!!!!
+            //Писать модификатор доступа писать надо!
+            //Public пишут вверху программы, далее пишут private меиоды
+
+            //private static void PrintNumber(int number)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //private static void PrintNumbers(int[] numbers)
+            //{
+            //    for (int i = 0; i < numbers.Length; i++)
+            //    {
+            //        if (i == 1)
+            //        {
+            //            continue;
+            //        }
+            //        if (i > 9)
+            //        {
+            //            break;
+            //        }
+            //        Console.WriteLine(numbers[i]);
+            //    }
+
+            //}
+
+            //private static void PrintNumbers2(params int[] numbers)
+            //{
+            //    for (int i = 0; i < numbers.Length; i++)
+            //    {
+            //        if (i == 1)
+            //        {
+            //            continue;
+            //        }
+            //        if (i > 9)
+            //        {
+            //            break;
+            //        }
+            //        Console.WriteLine(numbers[i]);
+            //    }
+
+            //}
+
+            //private static void PrintString(string str)
+            //{
+            //    if (str == "" || str == null)
+            //    {
+            //        return;
+            //    }
+            //    Console.WriteLine(str);
+            //}
+
+            //in параметр используется для чистой функции, онли чтение!
+            private static int Sum(ref int a, int b)
+            {
+                a = 10;
+                int result = a + b;
+
+                return result;
+            }
+
+            //public static float Devide(float a, float b)
+            //{
+            //    if (b != 0)
+            //    {
+            //        return a / b;
+            //    }
+            //    else
+            //    {
+            //        return 0;
+            //    }
+
+            //}
+            //public static bool TryDevide(decimal a, decimal b, out decimal result)
+            //{
+            //    result = 0;
+
+            //    if (b <= 0)
+            //    {
+            //        return false;
+            //    }
+
+            //    result = a / b;
+            //    return true;
+
+            //}
+
+            static void Main(string[] args)
+            {
+
+
+            //PrintNumbers2(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            //PrintNumbers2(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            int number = 24;
+            int result = Sum(ref number, 7);
+            Console.WriteLine(result);
+            Console.WriteLine(number);
+
+            number = 24;
+            int result2 = Sum(ref number, 22);
+            Console.WriteLine(result2);
+            Console.WriteLine(number);
+
+            //bool isSuccess = TryDevide(10, 0, out decimal result);
+            //if (isSuccess)
+            //{
+            //    Console.WriteLine(result);
+            //}
+
+            //isSuccess = TryDevide(10, 2, out result);
+            //if (isSuccess)
+            //{
+            //    Console.WriteLine(result);
+            //}
+
+            //string answer = Console.ReadLine();
+            //bool isSuccess = int.TryParse(answer, out int number);
+            //if (isSuccess)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            //PrintNumber(256);
+            //PrintString("Test string");
+            //PrintString(null);
+            //PrintString("");
+
+            //PrintNumbers(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            //float floatResult = Devide(4, 7);
+            //Console.WriteLine(floatResult);
+
+
+
+            }
+        
+    }
+}
